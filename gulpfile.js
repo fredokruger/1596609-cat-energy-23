d8b95cdbea6aff56aa5208fc2fbe59cc47b99b30
Keks <keks@htmlacademy.ru>
2021-07-28 18:36:04 +0300

    :kissing_cat: Добавляет сборку Sass на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
+const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
+const postcssUrl = require("postcss-url");
+const postcssImport = require("postcss-import");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/less/style.less")
+  return gulp.src("source/sass/style.scss", { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
+    .pipe(postcss([
+      postcssImport(),
+      postcssUrl(),
+    ]))
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("source/css", { sourcemaps: '.' }))
     .pipe(sync.stream());
 }
 
@@ -42,7 +45,7 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
