b061d7d22de7763195415c4fd2882237e5338ed6
Cozart <artemvlasov22@gmail.com>
2021-08-09 19:28:23 +0300

    Исправить регистр именования файлов шрифтов

--- source/js/map.js
+++ source/js/map.js
@@ -4,16 +4,16 @@ function initMap() {
   let markerImage;
   if (window.matchMedia("(min-width: 1280px)").matches) {
     centerCoordinates = { lat: 59.93883740459927, lng: 30.32066093815554 };
-    markerImage = '../img/map-marker-desktop.png';
+    markerImage = 'img/map-marker-desktop.png';
   } else if (window.matchMedia("(min-width: 768px)").matches) {
     centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
-    markerImage = '../img/map-marker-tablet.png';
+    markerImage = 'img/map-marker-tablet.png';
   } else {
     centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
-    markerImage = '../img/map-marker-mobile.png';
+    markerImage = 'img/map-marker-mobile.png';
   }
 
-  let map = new google.maps.Map(document.querySelector(".contacts__map"), {
+  map = new google.maps.Map(document.querySelector(".contacts__map"), {
     zoom: 15,
     disableDefaultUI: true,
     center: centerCoordinates,
