13db41bb7e36d777d057b0886dd6e5b96b3bfb67
Cozart <artemvlasov22@gmail.com>
2021-08-13 21:55:51 +0300

    Выполнить ретинизацию растровых изображений

--- source/js/map.js
+++ source/js/map.js
@@ -2,15 +2,25 @@ function initMap() {
   let coordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
   let centerCoordinates;
   let markerImage;
-  if (window.matchMedia("(min-width: 1280px)").matches) {
+  let retina = window.devicePixelRatio > 1;
+  if (window.matchMedia("(min-width: 1280px)").matches && retina) {
     centerCoordinates = { lat: 59.93883740459927, lng: 30.32066093815554 };
-    markerImage = 'img/map-marker-desktop.png';
+    markerImage = 'img/map-marker-desktop@2x.png';
+  } else if (window.matchMedia("(min-width: 1280px)").matches) {
+    centerCoordinates = { lat: 59.93883740459927, lng: 30.32066093815554 };
+    markerImage = 'img/map-marker-tablet@1x.png';
   } else if (window.matchMedia("(min-width: 768px)").matches) {
     centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
-    markerImage = 'img/map-marker-tablet.png';
+    markerImage = 'img/map-marker-tablet@1x.png';
+  } else if (window.matchMedia("(min-width: 768px)").matches && retina) {
+    centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
+    markerImage = 'img/map-marker-tablet@2x.png';
+  } else if (retina) {
+    centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
+    markerImage = 'img/map-marker-mobile@2x.png';
   } else {
     centerCoordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
-    markerImage = 'img/map-marker-mobile.png';
+    markerImage = 'img/map-marker-mobile@1x.png';
   }
 
   map = new google.maps.Map(document.querySelector(".contacts__map"), {
