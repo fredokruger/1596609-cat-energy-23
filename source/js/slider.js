2e4c5afedbbf8f5dc03e373f21a674d16f6f1d78
Cozart <artemvlasov22@gmail.com>
2021-08-25 19:26:32 +0300

    Реализовать работу слайдера для всех устройств

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,71 +2,104 @@ const imageContainer = document.querySelector('.slider__images-container');
 const change = document.querySelector('.slider__handle');
 const body = document.body;
 const scale = document.querySelector('.slider__scale');
+const ConditionBefore = document.querySelector('.slider__condition--before');
+const ConditionAfter = document.querySelector('.slider__condition--after');
 
-let isActive = false;
-
-change.addEventListener('mousedown', () => {
-  isActive = true;
+ConditionBefore.addEventListener('click', function () {
+  imageContainer.style.setProperty('--curtain-position', '100%');
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    change.style.left = '0';
+  } else {
+    change.style.transform = 'translateX(0)'
+  };
 });
 
-window.addEventListener('mouseup', () => {
-  isActive = false;
-});
+ConditionAfter.addEventListener('click', function () {
+  imageContainer.style.setProperty('--curtain-position', '0%');
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    change.style.left = '100%';
+  } else {
+    change.style.transform = 'translateX(100%)'
+  };
+})
 
-body.addEventListener('mouseleave', () => {
-  isActive = true;
-});
+if (window.matchMedia("(min-width: 768px)").matches) {
+  let isActive = false;
 
-const beforeAfterSlider = (x) => {
-  let shift = Math.max(0, Math.min(x, scale.offsetWidth));
-  change.style.left = 100 * shift / scale.offsetWidth + '%';
-  let width = 100 - 100 * shift / scale.offsetWidth + '%';
-  imageContainer.style.setProperty('--curtain-position', width);
-};
+  change.addEventListener('mousedown', () => {
+    isActive = true;
+  });
 
-const pauseEvents = (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-  return false;
-};
+  window.addEventListener('mouseup', () => {
+    isActive = false;
+  });
 
-body.addEventListener('mousemove', (e) => {
-  if (!isActive) {
-    return;
-  }
+  body.addEventListener('mouseleave', () => {
+    isActive = true;
+  });
 
-  let x = e.pageX;
-  x -= scale.getBoundingClientRect().left;
-  beforeAfterSlider(x);
-  pauseEvents(e);
-});
+  const beforeAfterSlider = (x) => {
+    let shift = Math.max(0, Math.min(x, scale.offsetWidth));
+    change.style.left = 100 * shift / scale.offsetWidth + '%';
+    let width = 100 - 100 * shift / scale.offsetWidth + '%';
+    imageContainer.style.setProperty('--curtain-position', width);
+  };
 
-change.addEventListener('touchstart', () => {
-  isActive = true;
-});
+  const pauseEvents = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+  };
 
-body.addEventListener('touchend', () => {
-  isActive = false;
-});
+  body.addEventListener('mousemove', (e) => {
+    if (!isActive) {
+      return;
+    }
 
-body.addEventListener('touchcancel', () => {
-  isActive = false;
-});
+    let x = e.pageX;
+    x -= scale.getBoundingClientRect().left;
+    beforeAfterSlider(x);
+    pauseEvents(e);
+  });
 
-body.addEventListener('touchmove', (e) => {
-  if (!isActive) {
-    return;
-  }
+  change.addEventListener('touchstart', () => {
+    isActive = true;
+  });
 
-  let x;
+  body.addEventListener('touchend', () => {
+    isActive = false;
+  });
 
-  let i;
-  for (i = 0; i < e.changedTouches.length; i++) {
-    x = e.changedTouches[i].pageX;
-  }
+  body.addEventListener('touchcancel', () => {
+    isActive = false;
+  });
 
-  x -= scale.getBoundingClientRect().left;
+  body.addEventListener('touchmove', (e) => {
+    if (!isActive) {
+      return;
+    }
 
-  beforeAfterSlider(x);
-  pauseEvents(e);
-});
+    let x;
+
+    let i;
+    for (i = 0; i < e.changedTouches.length; i++) {
+      x = e.changedTouches[i].pageX;
+    }
+
+    x -= scale.getBoundingClientRect().left;
+
+    beforeAfterSlider(x);
+    pauseEvents(e);
+  });
+} else {
+
+  scale.addEventListener('click', function () {
+    if (change.style.transform === 'translateX(100%)') {
+      change.style.transform = 'translateX(0)'
+      imageContainer.style.setProperty('--curtain-position', '100%');
+    } else {
+      change.style.transform = 'translateX(100%)'
+      imageContainer.style.setProperty('--curtain-position', '0%');
+    }
+  });
+}
