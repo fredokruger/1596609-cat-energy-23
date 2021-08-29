e1ff0ac5c771d8204a5d44217a82880e5a7865fd
Cozart <artemvlasov22@gmail.com>
2021-08-29 12:51:23 +0300

    Оптимизировать работу карты при ресайзе страницы

--- source/js/map.js
+++ source/js/map.js
@@ -1,7 +1,18 @@
 function initMap() {
   let coordinates = { lat: 59.938916705952764, lng: 30.322972921767455 };
   let centerCoordinates;
-  let markerImage = 'img/map-pin.svg';
+  let icon;
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    icon = {
+      url: 'img/map-pin.svg',
+      scaledSize: new google.maps.Size(89, 104)
+    };
+  } else {
+    icon = {
+      url: 'img/map-pin.svg',
+      scaledSize: new google.maps.Size(60, 69),
+    };
+  }
   if (window.matchMedia("(min-width: 1280px)").matches) {
     centerCoordinates = { lat: 59.93883740459927, lng: 30.32066093815554 };
   } else {
@@ -17,6 +28,10 @@ function initMap() {
   marker = new google.maps.Marker({
     position: coordinates,
     map: map,
-    icon: markerImage
+    icon: icon,
   });
+
 }
+window.addEventListener('resize', () => {
+  initMap()
+})
